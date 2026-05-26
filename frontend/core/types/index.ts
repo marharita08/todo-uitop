1dd3dcd615a62f6080abe36ef35fe557f6756078
marharita08 <rozgon.margarita08@gmail.com>
2026-05-26 14:53:11 +0300

    add basic components, services and hooks

---
+++ frontend/core/types/index.ts
@@ -0,0 +1,3 @@
+export * from './category.types';
+export * from './todo.types';
+export * from './pagination.types';
---
+++ frontend/services/todo.service.ts
@@ -0,0 +1,26 @@
+import { CreateTodoDto, UpdateTodoDto } from '@/core/schemas';
+import { Paginated, Todo, TodoFilter } from '@/core/types';
+
+import { httpService } from './http.service';
+
+class TodoService {
+  private readonly baseUrl = '/todos';
+
+  async getAll(filter?: TodoFilter): Promise<Paginated<Todo>> {
+    return httpService.get<Paginated<Todo>>(this.baseUrl, filter);
+  }
+
+  async create(dto: CreateTodoDto): Promise<Todo> {
+    return httpService.post<Todo, CreateTodoDto>(this.baseUrl, dto);
+  }
+
+  async update(id: number, dto: UpdateTodoDto): Promise<Todo> {
+    return httpService.patch<Todo, UpdateTodoDto>(`${this.baseUrl}/${id}`, dto);
+  }
+
+  async remove(id: number): Promise<{ message: string }> {
+    return httpService.delete<{ message: string }>(`${this.baseUrl}/${id}`);
+  }
+}
+
+export const todoService = new TodoService();
